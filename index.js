292560d1a4654d07856f1c5ee6f4bb1e7d7604e0
kasif Ali <kasifali009@gmail.com>
2023-10-17 02:45:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+gsap.from(".logo", { opacity: 0, duration: 1, delay: 2, y: 10 });
+gsap.from(".list a", {
+  opacity: 0,
+  duration: 1,
+  delay: 2.1,
+  y: 30,
+  stagger: 0.2,
+});
+
+gsap.from(".main", { opacity: 0, duration: 1, delay: 1.6, y: 30 });
+gsap.from(".image-2", { opacity: 0, duration: 1, delay: 1.8, y: 30 });
+
